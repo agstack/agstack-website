a4303c34898619350e746a52abd781b68747306e
Mikela Jackson <mdockery@redhat.com>
2022-12-13 19:17:09 -0500

    Update swagger-initializer.js
    adding comment

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -2,6 +2,7 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  //based on instructions, we need to replace the url parameter with the link to the local file (user-registry.yaml)
   window.ui = SwaggerUIBundle({
     url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
